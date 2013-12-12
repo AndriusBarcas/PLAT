877b958b0444ed2f29c7eaa12b1bb2bc051ae528
AndriusBarcas <Andriuss1991@gmail.com>
2013-12-12 22:29:12 +0200

    Update Daiktai.java

--- src/Entity/Daiktai.java
+++ src/Entity/Daiktai.java
@@ -4,6 +4,7 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 import Manager.Content;
+import Manager.GameStateManager;
 import tilemap.TileMap;
 
 
@@ -11,32 +12,47 @@ public class Daiktai extends Entity{
 
 	private BufferedImage sprite;
 	private int type;
-	public static final int BOW = 0;
-	public static final int AXE = 1;
+	public static final int BOOTS = 0;
+	public static final int POTION = 1;
+	public static final int WINGS = 2;
+	public static final int CROWN = 3;
+	
 	
 	public Daiktai(TileMap tm) {
 		super(tm);
 		type = -1;
-		width = height = 16;
-		cwidth = cheight = 12;
+		width = height = 30;
+		cwidth = cheight = 30;
 	}
 	
 	public void setType(int i) {
 		type = i;
-		if(type == BOW) {
-          	sprite = Content.DAIKTAI2[0][0];
+		if(type == POTION) {
+          	sprite = Content.DAIKTAI[0][1];
 		}
-		else if(type == AXE) {
+		if(type == BOOTS) {
+			sprite = Content.DAIKTAI[0][0];
+		}
+		if(type == WINGS){
+			sprite = Content.DAIKTAI[1][0];
+		}
+		if(type == CROWN){
 			sprite = Content.DAIKTAI[1][1];
 		}
 	}
 	
-	public void collected(zaidejas p) {
-		if(type == BOW) {
-			p.gotBow();
+	public void collected(GameStateManager gsm, zaidejas p) {
+		if(type == BOOTS){
+			p.addEjimai(10);
 		}
-		if(type == AXE) {
-     		p.gotAxe();
+		if(type == POTION){
+			p.addEjimai(5);
+		}
+		if(type == WINGS){
+			p.addEjimai(20);
+		}
+		if(type == CROWN){
+			gsm.setState(GameStateManager.END);
 		}
 	}
 	
